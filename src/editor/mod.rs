a5ce4b8208d3fc1713db6f50c9cc34f66a08baa7
zuiyu1998 <1542844298@qq.com>
2024-02-21 16:10:41 +0800

    feat: 添加编译器

--- src/editor/mod.rs
+++ src/editor/mod.rs
@@ -1,10 +1,15 @@
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::*;
+use bevy_editor_pls::prelude::*;
 
 pub struct InternalEditorPlugin;
 
 impl Plugin for InternalEditorPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin::default()));
+        app.add_plugins((
+            FrameTimeDiagnosticsPlugin,
+            LogDiagnosticsPlugin::default(),
+            EditorPlugin::default(),
+        ));
     }
 }
